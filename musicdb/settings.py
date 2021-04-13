efb5c1b8a7df79ca94bddd57b42bc6d6815a8499
Jon Thole <jthole87@gmail.com>
2021-04-12 21:54:50 -0600

    Fix pulling env variables.

--- musicdb/settings.py
+++ musicdb/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 
-SECRET_KEY = os.environ.get['MDB_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag']
-ENV = os.environ['MDB_ENV', 'DEV']
+SECRET_KEY = os.environ.get('MDB_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+ENV = os.environ.get('MDB_ENV', 'DEV')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
